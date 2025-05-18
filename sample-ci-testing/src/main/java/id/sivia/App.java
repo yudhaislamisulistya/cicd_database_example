a047f431b44b2001e00429ed52d202e92040cb2a
Yudha Islami Sulistya <yudhamaster77@gmail.com>
2025-05-19 01:12:12 +0700

    Remove logger initialization and logging statement from App class

--- sample-ci-testing/src/main/java/id/sivia/App.java
+++ sample-ci-testing/src/main/java/id/sivia/App.java
@@ -4,9 +4,6 @@ import java.util.logging.Logger;
 
 public class App {
 
-    private static final Logger logger = Logger.getLogger(App.class.getName());
-
     public static void main(String[] args) {
-        logger.info("Hello World!");
     }
 }
