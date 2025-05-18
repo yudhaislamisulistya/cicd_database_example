c376e5e229f708558c0fbb44ce9e0140e590c3a5
Yudha Islami Sulistya <yudhamaster77@gmail.com>
2025-05-19 01:46:13 +0700

    Refactor SonarQube environment variables in docker-compose.yml and add longParameterList method in App.java

--- sample-ci-testing/src/main/java/id/sivia/App.java
+++ sample-ci-testing/src/main/java/id/sivia/App.java
@@ -8,4 +8,10 @@ public class App {
         }
         System.out.println("Sum of first 10 numbers is: " + sum);
     }
+
+    public void longParameterList(String a, String b, String c, String d, String e, String f, String g, String h,
+            String i, String j) {
+        System.out.println("Long parameter list: " + a + ", " + b + ", " + c + ", " + d + ", " + e + ", " + f + ", " + g
+                + ", " + h + ", " + i + ", " + j);
+    }
 }
