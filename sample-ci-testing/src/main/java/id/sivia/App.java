b19a6341a188d87f0e7e3469b22e3b16ce865fa0
Yudha Islami Sulistya <yudhamaster77@gmail.com>
2025-05-19 01:23:10 +0700

    Refactor main method to always calculate and print the sum of the first 10 numbers

--- sample-ci-testing/src/main/java/id/sivia/App.java
+++ sample-ci-testing/src/main/java/id/sivia/App.java
@@ -2,12 +2,10 @@ package id.sivia;
 
 public class App {
     public static void main(String[] args) {
-        // Tambahkan sedikit logic
-        if (args.length == 0) {
-            int sum = 0;
-            for (int i = 1; i <= 10; i++) {
-                sum += i;
-            }
+        int sum = 0;
+        for (int i = 1; i <= 10; i++) {
+            sum += i;
         }
+        System.out.println("Sum of first 10 numbers is: " + sum);
     }
 }
