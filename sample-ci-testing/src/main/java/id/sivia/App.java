101410857c7616b92d13f8daf3568d5fabd63fd8
Yudha Islami Sulistya <yudhamaster77@gmail.com>
2025-05-19 01:19:58 +0700

    Implement logic to calculate the sum of numbers from 1 to 10 when no arguments are provided

--- sample-ci-testing/src/main/java/id/sivia/App.java
+++ sample-ci-testing/src/main/java/id/sivia/App.java
@@ -4,7 +4,10 @@ public class App {
     public static void main(String[] args) {
         // Tambahkan sedikit logic
         if (args.length == 0) {
-            return;
+            int sum = 0;
+            for (int i = 1; i <= 10; i++) {
+                sum += i;
+            }
         }
     }
 }
